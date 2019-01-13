86d75d5508546084929d8dcd438a3397a4d96403
leedor B <leedorb@gmail.com>
2019-01-13 11:20:13 +0200

    test git connection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -20,6 +19,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <div>testin conntection to git</div>
       </div>
     );
   }
